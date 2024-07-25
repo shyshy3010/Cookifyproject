31ae1f5dc3b74c2bbec62a6681e1c505ab9a4ea6
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-07-25 16:47:52 +0300

    integrate a new routers

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const loginRoutes = require('./routers/loginRoutes'); // Importer les routes de connexion
+const loginRoutes = require('.backend/routers/loginRoutes'); 
+const RecipesChefRoutes = require('.backend/routers/RecipesChefRoutes'); 
 
 const app = express();
 const PORT = 3021;
 
-// Middleware pour analyser les corps des requêtes en JSON
+
 app.use(bodyParser.json());
 
-// Configuration pour servir des fichiers statiques
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-// Utilisation des routes pour les connexions
 app.use('/api', loginRoutes);
 
-// Démarrer le serveur
+app.use('/api', RecipesChefRoutes);
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
