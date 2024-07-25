76c4a8c4a1b886db3d05a1380cdb83c2ab17a63c
shyshy3010 <journo.shyrel@gmail.com>
2024-07-25 15:58:18 +0300

    connextion of database sql

---
+++ backend/database.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+require('dotenv').config({ path: __dirname + '/.env' });
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Connexion à la base de données
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+module.exports = connection;
